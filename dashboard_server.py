098a6df6a141ad88e04643a1337ca385a72c7e87
John <john.l.hansen@gmail.com>
2026-07-15 23:46:45 -0700

    fix: remove duplicate Allow-Origin header to resolve browser CORS block

--- dashboard_server.py
+++ dashboard_server.py
@@ -79,7 +79,6 @@ class DashboardRequestHandler(SimpleHTTPRequestHandler):
 
     def do_OPTIONS(self):
         self.send_response(200)
-        self.send_header("Access-Control-Allow-Origin", "*")
         self.send_header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
         self.send_header("Access-Control-Allow-Headers", "Content-Type")
         self.end_headers()
